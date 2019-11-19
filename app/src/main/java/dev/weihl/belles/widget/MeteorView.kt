5d4bac1a7c7698d9c4cb329d36bfc59b4bad6448
Weihl <943334068@qq.com>
2019-11-19 22:26:34 +0800

    - Lifecycle

--- app/src/main/java/dev/weihl/belles/widget/MeteorView.kt
+++ app/src/main/java/dev/weihl/belles/widget/MeteorView.kt
@@ -12,6 +12,10 @@ import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.widget.FrameLayout
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import dev.weihl.belles.screens.BasicActivity
 import timber.log.Timber
 
 /**
@@ -37,6 +41,27 @@ class MeteorView(context: Context, attrs: AttributeSet?) : FrameLayout(context, 
         Timber.tag("MeteorView")
         Timber.d(" Init !")
         background = ColorDrawable(BLACK)
+        loopRunnable = Runnable {
+            addAnimatorSet(newStar()).start()
+            handler.postDelayed(loopRunnable, LOOP_DURATION)
+        }
+        if (context is BasicActivity) {
+            context.lifecycle.addObserver(object : LifecycleObserver {
+
+                @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+                fun onPause() {
+                    loopPause()
+                }
+
+                @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+                fun onResume() {
+                    loopResume()
+                }
+
+            })
+
+        }
+
     }
 
 
@@ -47,27 +72,20 @@ class MeteorView(context: Context, attrs: AttributeSet?) : FrameLayout(context, 
         Timber.d("MeteorView onSizeChanged !")
     }
 
+    fun loopResume() {
+        Timber.d("MeteorView loopResume !")
+        handler?.postDelayed(loopRunnable, LOOP_DURATION)
+    }
+
+    fun loopPause() {
+        Timber.d("MeteorView loopPause !")
+        handler?.removeCallbacks(loopRunnable)
+    }
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        loopRunnable = Runnable {
-            addAnimatorSet(newStar()).start()
-            loopAddStar()
-        }
-        loopAddStar()
-        Timber.d("MeteorView onAttachedToWindow !")
+        loopResume()
     }
-
-
-    private fun loopAddStar() {
-        handler.postDelayed(loopRunnable, LOOP_DURATION)
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        handler.removeCallbacks(loopRunnable)
-        Timber.d("MeteorView onDetachedFromWindow !")
-    }
-
 
     private fun newStar(): AppCompatImageView {
         var scale = (Math.random() * 2.0f + .1f).toFloat()
