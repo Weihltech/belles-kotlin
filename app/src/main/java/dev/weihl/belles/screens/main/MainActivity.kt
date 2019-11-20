daa61ede19601e43a14cfcf58af6d1f57c861198
Weihl <943334068@qq.com>
2019-11-20 21:33:30 +0800

    - Lifecycle

--- app/src/main/java/dev/weihl/belles/screens/main/MainActivity.kt
+++ app/src/main/java/dev/weihl/belles/screens/main/MainActivity.kt
@@ -1,13 +1,20 @@
 package dev.weihl.belles.screens.main
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import dev.weihl.belles.R
+import dev.weihl.belles.databinding.ActivityMainBinding
 import dev.weihl.belles.screens.BasicActivity
 
 class MainActivity : BasicActivity() {
 
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button
     }
 }
---
+++ app/src/main/java/dev/weihl/belles/screens/main/MainModel.kt
@@ -0,0 +1,13 @@
+package dev.weihl.belles.screens.main
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Des:
+ * Created by Weihl
+ * 2019-11-19
+ */
+
+class MainModel : ViewModel(){
+
+}
