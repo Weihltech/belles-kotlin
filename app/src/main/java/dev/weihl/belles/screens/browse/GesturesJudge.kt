e7d74078b343fe41a2d26803c4edb983970d430b
Weihongliu <weihongliu@vsoontech.com>
2020-11-20 12:04:32 +0800

    m- ?

---
+++ app/src/main/java/dev/weihl/belles/screens/browse/GesturesJudge.kt
@@ -0,0 +1,57 @@
+package dev.weihl.belles.screens.browse
+
+import android.view.MotionEvent
+import timber.log.Timber
+import kotlin.math.abs
+
+/**
+ * 垂直手势辅助判断
+ */
+
+private val eventDown = arrayOf(0f, 0f)
+private val eventMove = arrayOf(0f, 0f)
+private var verticalSliding = false
+private var hasJudgeSliding = false
+fun judgeTouchEvent(event: MotionEvent?): Boolean {
+
+    when (event?.action) {
+        MotionEvent.ACTION_DOWN -> {
+            eventDown[0] = event.x
+            eventDown[1] = event.y
+            Timber.d("ACTION_DOWN ${eventDown.contentToString()}")
+        }
+        MotionEvent.ACTION_UP -> {
+            hasJudgeSliding = false
+            verticalSliding = false
+            Timber.d("ACTION_UP   ")
+        }
+        MotionEvent.ACTION_MOVE -> {
+            eventMove[0] = event.x
+            eventMove[1] = event.y
+
+            if (!hasJudgeSliding) {
+                val offsetX = abs(eventDown[0] - eventMove[0])
+                val offsetY = abs(eventDown[1] - eventMove[1])
+
+                Timber.d("ACTION_MOVE ${eventMove.contentToString()} ； [ offsetX：$offsetX , offsetY:$offsetY ]")
+                // 单位计算 >10
+                if (offsetX > 10 || offsetY > 10) {
+                    hasJudgeSliding = true
+                    if (offsetY - offsetX > 2) {
+                        // 此时，垂直滑动，角度 大于 45°；
+                        Timber.d("ACTION_MOVE  is 垂直滑动 !")
+                        verticalSliding = true
+                    }
+                }
+            }
+
+        }
+    }
+
+    if (hasJudgeSliding && verticalSliding) {
+        Timber.d("ACTION_MOVE(垂直控制) :${eventMove.contentToString()}")
+        return true
+    }
+
+    return false
+}
--- app/src/main/java/dev/weihl/belles/screens/browse/PhotosActivity.kt
+++ app/src/main/java/dev/weihl/belles/screens/browse/PhotosActivity.kt
@@ -7,7 +7,6 @@ import dev.weihl.belles.data.json2SexyImageList
 import dev.weihl.belles.screens.BasicActivity
 import kotlinx.android.synthetic.main.activity_photos.*
 import timber.log.Timber
-import kotlin.math.abs
 
 class PhotosActivity : BasicActivity() {
 
@@ -44,53 +43,8 @@ class PhotosActivity : BasicActivity() {
         }
     }
 
-
-    private val eventDown = arrayOf(0f, 0f)
-    private val eventMove = arrayOf(0f, 0f)
-    private var verticalSliding = false
-    private var hasJudgeSliding = false
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
-
-        when (event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                eventDown[0] = event.x
-                eventDown[1] = event.y
-                Timber.d("ACTION_DOWN ${eventDown.contentToString()}")
-            }
-            MotionEvent.ACTION_UP -> {
-                hasJudgeSliding = false
-                verticalSliding = false
-                Timber.d("ACTION_UP   ")
-            }
-            MotionEvent.ACTION_MOVE -> {
-                eventMove[0] = event.x
-                eventMove[1] = event.y
-
-                if (!hasJudgeSliding) {
-                    val offsetX = abs(eventDown[0] - eventMove[0])
-                    val offsetY = abs(eventDown[1] - eventMove[1])
-
-                    Timber.d("ACTION_MOVE ${eventMove.contentToString()} ； [ offsetX：$offsetX , offsetY:$offsetY ]")
-                    // 单位计算 >10
-                    if (offsetX > 10 || offsetY > 10) {
-                        hasJudgeSliding = true
-                        if (offsetY - offsetX > 2) {
-                            // 此时，垂直滑动，角度 大于 45°；
-                            Timber.d("ACTION_MOVE  is 垂直滑动 !")
-                            verticalSliding = true
-                        }
-                    }
-                }
-
-            }
-        }
-
-        if (hasJudgeSliding && verticalSliding) {
-            Timber.d("ACTION_MOVE(垂直控制) :${eventMove.contentToString()}")
-            return true
-        }
-
-        return super.dispatchTouchEvent(event)
+        return judgeTouchEvent(event) || super.dispatchTouchEvent(event)
     }
 
     override fun onBackPressed() {
